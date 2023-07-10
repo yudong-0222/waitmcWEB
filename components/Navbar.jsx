76fa491e6cdc72b5d1f8b99b93e50f84c533b4ba
RedFeaTherXll <yuchi9716@gmail.com>
2023-07-10 12:53:10 +0800

    change amount of viewport navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
       variants={navVariants}
       initial="hidden"
       whileInView="show"
-      viewport={{ once: true, amount: 0.5 }}
+      viewport={{ once: true, amount: 0.25 }}
       className={`${styles.xPaddings} py-8 relative`}
     >
       <div className={`${styles.innerWidth} mx-auto flex items-start gap-8`}>
