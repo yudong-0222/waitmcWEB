4ce054e951cba3f7a76b9e26ac7a2e9aa556b3fe
YuDong <yuchi9716@gmail.com>
2025-02-22 03:08:37 +0800

    Updating NavBar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -16,31 +16,38 @@ const Navbar = () => {
       viewport={{ once: true, amount: 0.25 }}
       className={`${styles.xPaddings} py-8 relative`}
     >
-      <div className={`${styles.innerWidth} mx-auto flex items-start gap-8`}>
-        <a href="/" className="z-9999">
-          <h2 className="font-extrabold text-[24px] leading-[30.24px] text-white cursor-pointer z-9999">
-            WAIT NETWORK
-          </h2>
-        </a>
-        <ul className="list-none sm:flex hidden justify-end items-center flex-1 text-white text-extrabold">
-          {navLinks.map((nav, index) => (
-            <li
-              key={nav.id}
-              className={`font-poppins font-normal cursor-pointer text-[16px] hover:border-y-2 border-indigo-500 ${
-                active === nav.title ? 'text-white' : 'text-dimWhite'
-              } ${index === navLinks.length - 1 ? 'mr-0' : 'mr-10'}`}
-              onClick={() => setActive(nav.title)}
-            >
-              <a href={`${nav.id}`}>{nav.title}</a>
+      <div className="flex items-start justify-between gap-8">
+        {/* Logo */}
+        <div className="flex flex-row items-center">
+          <a href="/" className="flex flex-row items-center space-x-2 z-[9999]">
+            <img src="https://i.imgur.com/Nphfk7A.png" className="w-[50px] h-[50px] object-contain" />
+            <h2 className="font-extrabold text-[24px] leading-[30.24px] text-white cursor-pointer z-[9999]">
+              WAIT NETWORK
+            </h2>
+          </a>
+        </div>
+        {/* List Nav */}
+        <div className="hidden sm:hidden md:hidden lg:flex xl:flex">
+          <ul className="list-none flex items-center text-white text-extrabold">
+            {navLinks.map((nav, index) => (
+              <li
+                key={nav.id}
+                className={`font-poppins font-normal cursor-pointer text-[16px] hover:border-y-2 border-indigo-500 ${
+                  active === nav.title ? 'text-white' : 'text-dimWhite'
+                } ${index === navLinks.length - 1 ? 'mr-0' : 'mr-10'}`}
+                onClick={() => setActive(nav.title)}
+              >
+                <a href={`${nav.id}`}>{nav.title}</a>
+              </li>
+            ))}
+            <li className="font-poppins font-bold text-[32px] ml-10">
+              <span className="before:block before:absolute before:-inset-1 before:-skew-y-3 before:bg-pink-500 relative inline-block">
+                <span className="relative text-white">WAITMC.TOP</span>
+              </span>
             </li>
-          ))}
-          <li className="font-poppins font-bold text-[32px] ml-10">
-            <span className="before:block before:absolute before:-inset-1 before:-skew-y-3 before:bg-pink-500 relative inline-block">
-              <span className="relative text-white">WAITMC.TOP</span>
-            </span>
-          </li>
-        </ul>
-        <div className="sm:hidden flex flex-1 justify-end items-center">
+          </ul>
+        </div>
+        <div className="sm:flex md:flex lg:hidden xl:hidden flex justify-end items-end">
           <img
             src={toggle ? '/close.svg' : '/menu.svg'}
             alt="menu"
